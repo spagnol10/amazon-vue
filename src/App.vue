8306f1a592ff887acfaba2f0d11193bee457b69d
wellynton.spagnol <wellynton.spagnol@aliare.co>
2024-02-28 13:52:46 -0300

    Criando o formulario

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,24 @@
 <script setup>
 import FormLayouts from './components/FormLayouts.vue';
+import Login from './components/Login.vue';
+import Dashboard from './components/Dashboard.vue';
+import Form from './components/Form.vue';
+
 
 </script>
 
 <template>
   <div>
 
+
+    <!-- <Navbar /> -->
+
+    <!-- <Login /> -->
+
+    <!-- <Dashboard /> -->
+
     <FormLayouts />
 
+    <!-- <Form /> -->
   </div>
 </template>
