c9a075df7356f21fabcec12b9704bcf30984c9de
Wellynton.Spagnol <Wellyntonspagnol10@gmail.com>
2024-02-28 23:46:20 -0300

    Importei todos os componentes disponiveis do tailwind agora precisa revisar todos os componetes para ver se estão funcionando corretamente

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,17 @@
 import FormLayouts from './components/FormLayouts.vue';
 import Login from './components/Login.vue';
 import Dashboard from './components/Dashboard.vue';
+import ProductOverviews from './components/ProductOverviews.vue';
+import ProductLists from './components/ProductLists.vue';
+import CategoryPreviews from './components/CategoryPreviews.vue';
+import ShoppingCart from './components/ShoppingCarts.vue';
+import TeamSections from './components/TeamSections.vue';
+import StoreNavigation from './components/StoreNavigation.vue';
+import StackedLists from './components/StackedLists.vue';
+import SlideOvers from './components/SlideOvers.vue';
+
+
+
 
 
 </script>
@@ -9,6 +20,23 @@ import Dashboard from './components/Dashboard.vue';
 <template>
   <div>
 
+    <!-- <Slide-Overs /> -->
+
+    <!-- <SlideOvers /> -->
+
+    <!-- <StackedLists /> -->
+
+    <!-- <StoreNavigation /> -->
+
+    <!-- <TeamSections /> -->
+
+    <!-- <ShoppingCart /> -->
+
+    <!-- <CategoryPreviews /> -->
+
+    <!-- <ProductLists /> -->
+
+    <ProductOverviews />
 
     <!-- <Navbar /> -->
 
@@ -16,7 +44,7 @@ import Dashboard from './components/Dashboard.vue';
 
     <!-- <Dashboard /> -->
 
-    <FormLayouts />
+    <!-- <FormLayouts /> -->
 
   </div>
 </template>
